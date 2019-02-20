10b80a82187965e1c325303cbf293adf1e6d5b2e
LeslieZarate <fiorelly30zarate@gmail.com>
2019-02-19 23:54:28 -0500

    Avance de diseño de interfaz de usuario

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 /* Acá va tu código */
+document.getElementById("btnB").addEventListener("click",saludar);
+function saludar(){
+const name = document.getElementById('name').value;
+document.getElementById('saludo').innerHTML =`Bienvenido estas en ${name}`;
+document.getElementById("container1").style.display="none";
+document.getElementById("container2").style.display="block"; 
+}
+
+
+document.getElementById("btnReg").addEventListener("click",Registrar);
+function Registrar(){
+document.getElementById("container2").style.display="none";
+document.getElementById("container3").style.display="block";
+}
