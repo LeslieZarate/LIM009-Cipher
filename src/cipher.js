3622c2c80eb451313113b4120e8db7d172c795e6
LeslieZarate <fiorelly30zarate@gmail.com>
2019-02-21 11:26:54 -0500

    encriptar soluciinado

--- src/cipher.js
+++ src/cipher.js
@@ -1,38 +1,26 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
+  encode: (offset,string) => {
+		let numeroAscii;
+    let resultCad = "";
+//RECORRIDO EL MENSAJE INGRESADO
+		for (let i = 0; i < string.length; i++){
+			numeroAscii = string.charCodeAt(i);
+	// mayúsculas
+			if (numeroAscii >= 65 && numeroAscii <= 90){
+				numeroAscii = String.fromCharCode((numeroAscii - 65 + parseInt(offset)) % 26 + 65);
+				resultCad = resultCad + numeroAscii;
+			}
+		}
+		return resultCad
+
+	},
+
+	
+
   decode: () => {
     /* Acá va tu código */
-  }
-};
-
-const cifrar = (message,offset) => {
-let numAscii;
-let resultC = "";
-//RECORRIDO EL MENSAJE DEL USUARIO
-	for (let i = 0; i < message.length; i++){
-		numAscii = message.charCodeAt(i);
-//Asimila mayúsculas
-		if (numAscii >= 65 && numAscii <= 90){
-			numAscii = String.fromCharCode((numAscii - 65 + parseInt(offset)) % 26 + 65);
-			resultC = resultC + numAscii;
-//Asimila minisculas
-		} else if (numAscii >= 97 && numAscii <= 122){
-			numAscii = String.fromCharCode((numAscii - 97 + parseInt(offset)) % 26 + 97);
-			resultC = resultC + numAscii;
-//Asimila el espacio " "
-		} else if (numAscii === 32){
-			resultC = resultC + " ";
-		}
 	}
-		return resultC
-};
-
-// En este objeto declaro los metodos ENCODE y DECODE
-window.cipher = {
-  encode :cifrar,
-  decode :descifrar
+	
 };
 
 
--- src/index.js
+++ src/index.js
@@ -4,29 +4,14 @@
  function funcionCifrar(){
 
  	let menIngresado=document.getElementById("message").value;
- 	let offset=document.getElementById("offset").value;
+	let offset=document.getElementById("offset").value;
 
- 	/*cipher.encode(menIngresado,offset);
-
- 	let menCifrado=menIngresado.replace(menIngresado,window.cipher.encode(menIngresado,offset);
- 	document.getElementById("final-message").innerHTML = menCifrado;
- 	
+	document.getElementById("final-message").innerHTML= cipher.encode(offset,menIngresado);
 	
-	if(messageVal===""){
-		emptyMessage.innerHTML=`Ingresa un texto aqui..`;
-	}
-	else if(offsetVal===""){
-		emptyOffset.innerHTML=`Ingrese el nivel de seguridad aqui..`;
-		
-	}
-	else {
-		cipher.encode(messageVal,offsetVal);
-	}
-*/
-	document.getElementById("final-message").innerHTML= menIngresado;
-    document.getElementById("programa").style.display="none";
+	document.getElementById("programa").style.display="none";
     document.getElementById("resultado").style.display="block";
-   
+	 
+	
 
 }
 
