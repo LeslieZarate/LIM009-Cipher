86b4b3a18c137a080a6b5150b56d5e9f14de3c2e
LeslieZarate <fiorelly30zarate@gmail.com>
2019-02-21 00:21:50 -0500

    cambio interfaz

--- src/cipher.js
+++ src/cipher.js
@@ -6,3 +6,33 @@ window.cipher = {
     /* Acá va tu código */
   }
 };
+
+const cifrar = (message,offset) => {
+let numAscii;
+let resultC = "";
+//RECORRIDO EL MENSAJE DEL USUARIO
+	for (let i = 0; i < message.length; i++){
+		numAscii = message.charCodeAt(i);
+//Asimila mayúsculas
+		if (numAscii >= 65 && numAscii <= 90){
+			numAscii = String.fromCharCode((numAscii - 65 + parseInt(offset)) % 26 + 65);
+			resultC = resultC + numAscii;
+//Asimila minisculas
+		} else if (numAscii >= 97 && numAscii <= 122){
+			numAscii = String.fromCharCode((numAscii - 97 + parseInt(offset)) % 26 + 97);
+			resultC = resultC + numAscii;
+//Asimila el espacio " "
+		} else if (numAscii === 32){
+			resultC = resultC + " ";
+		}
+	}
+		return resultC
+};
+
+// En este objeto declaro los metodos ENCODE y DECODE
+window.cipher = {
+  encode :cifrar,
+  decode :descifrar
+};
+
+
--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
-/* Acá va tu código */
-document.getElementById("btnB").addEventListener("click",saludar);
-function saludar(){
-const name = document.getElementById('name').value;
-document.getElementById('saludo').innerHTML =`Bienvenido estas en ${name}`;
-document.getElementById("container1").style.display="none";
-document.getElementById("container2").style.display="block"; 
+/**** cifrar ****/
+
+ document.getElementById("btn-encode").addEventListener("click",funcionCifrar)
+ function funcionCifrar(){
+
+ 	let menIngresado=document.getElementById("message").value;
+ 	let offset=document.getElementById("offset").value;
+
+ 	/*cipher.encode(menIngresado,offset);
+
+ 	let menCifrado=menIngresado.replace(menIngresado,window.cipher.encode(menIngresado,offset);
+ 	document.getElementById("final-message").innerHTML = menCifrado;
+ 	
+	
+	if(messageVal===""){
+		emptyMessage.innerHTML=`Ingresa un texto aqui..`;
+	}
+	else if(offsetVal===""){
+		emptyOffset.innerHTML=`Ingrese el nivel de seguridad aqui..`;
+		
+	}
+	else {
+		cipher.encode(messageVal,offsetVal);
+	}
+*/
+	document.getElementById("final-message").innerHTML= menIngresado;
+    document.getElementById("programa").style.display="none";
+    document.getElementById("resultado").style.display="block";
+   
+
 }
 
 
-document.getElementById("btnReg").addEventListener("click",Registrar);
-function Registrar(){
-document.getElementById("container2").style.display="none";
-document.getElementById("container3").style.display="block";
-}
+/*Nuevo Mensaje*/
+ const newMessage = document.getElementById("new-message");
+ newMessage.addEventListener("click",nuevo);
+ function nuevo(){
+ 	location.reload()
+ }
+
+
+
+
+
+
