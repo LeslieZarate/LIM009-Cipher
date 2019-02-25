2386a73baf2b2eb28a2739e67710c9d3cbcdc91e
LeslieZarate <fiorelly30zarate@gmail.com>
2019-02-25 13:02:58 -0500

    test avance

--- src/cipher.js
+++ src/cipher.js
@@ -5,15 +5,23 @@ window.cipher = {
 	//RECORRIDO EL MENSAJE INGRESADO
 		for (let i = 0; i < string.length; i++){
 				numeroAscii = string.charCodeAt(i);
-		// mayúsculas
+		/// mayúsculas
 				if (numeroAscii >= 65 && numeroAscii <= 90){
 					numeroAscii = String.fromCharCode((numeroAscii - 65 + parseInt(offset)) % 26 + 65);
 					resultCif = resultCif + numeroAscii;
 				}
+		//minusculas
 				else if (numeroAscii >= 97 && numeroAscii <= 122){
 					numeroAscii = String.fromCharCode((numeroAscii - 97 + parseInt(offset)) % 26 + 97);
 					resultCif = resultCif + numeroAscii;
-				}		
+				}
+		/*//numeros
+				else if(numeroAscii>=48 && numeroAscii <=57){
+					numeroAscii = String.fromCharCode((numeroAscii - 48 + parseInt(offset)) % 26 + 57);
+					resultCif = resultCif + numeroAscii;
+
+				}
+		*/
 			}
 			return resultCif
 
--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
-/**** cifrar ****/
 /*
-function palabra(){
+ document.getElementById("btn-encode").addEventListener("click",cifrar=()=>{
 	let menIngresado=document.getElementById("message").value;
-	let menSeparado=[];
-
-	for(let i;i<menIngresado.lenght;i++){
-		menSeparado += menIngresado
-	} 
-
-}
-*/
-
- document.getElementById("btn-encode").addEventListener("click",funcionCifrar)
- function funcionCifrar(){
-
- 	let menIngresado=document.getElementById("message").value;
 	let offset=document.getElementById("offset").value;
-
 
 	document.getElementById("final-message").innerHTML= cipher.encode(offset,menIngresado);	
 	document.getElementById("programa").style.display="none";
     document.getElementById("resultado").style.display="block";
-}
 
+ });
+ 
 document.getElementById("btn-decode").addEventListener("click",funcionDescifrar);
 function funcionDescifrar(){
 	let menIngresado=document.getElementById("message").value;
@@ -33,6 +19,22 @@ function funcionDescifrar(){
     document.getElementById("resultado").style.display="block";
 
 }
+*/
+
+const opciones=document.getElementById("botones");
+opciones.addEventListener("click",(e)=>{
+	let menIngresado=document.getElementById("message").value;
+	let offset=document.getElementById("offset").value;
+	if(e.target.id ==="btn-encode"){
+		document.getElementById("final-message").innerHTML= cipher.encode(offset,menIngresado);}
+	else if(e.target.id==="btn-decode"){
+		document.getElementById("final-message").innerHTML= cipher.decode(offset,menIngresado);
+	}
+	document.getElementById("programa").style.display="none";
+    document.getElementById("resultado").style.display="block";
+
+
+});
 
 
 /*Nuevo Mensaje*/
